0500ac06dc2ca8383f727f273215b69f3b402f16
Yury Shapkarin <yury@shapkarin.me>
2019-02-28 16:11:48 +0300

    fetch worker

---
+++ src/utils/fetch.js
@@ -0,0 +1,31 @@
+import { put, call, fork, takeEvery } from 'redux-saga/effects';
+
+import {
+    loadSomeDataStart,
+    loadSomeDataSucces,
+    loadSomeDataError
+} from './actions';
+
+export default function* fetch ({ action, method, start, succes, error }){
+    try {
+        yield put(start());
+        const { data } = yield call(method, action);
+        yield put(succes(data));
+    } catch (err) {
+        yield put(error(err));
+    }
+}
+
+function* fetchSomeData (action) {
+    yield fork(fetch, {
+        action,
+        method: fetchSomeData,
+        start: loadSomeDataStart,
+        succes: loadSomeDataSucces,
+        error: loadSomeDataError
+    });
+}
+
+function* mySaga () {
+    yield takeEvery('GET_SOME_DATA', fetchSomeData);
+}
