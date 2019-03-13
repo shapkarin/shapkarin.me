58a6f95236d073d02569000d033c02013d5d2d76
Yury Shapkarin <yury@shapkarin.me>
2019-03-13 19:06:47 +0300

    Update fetch.js

--- src/fetch.js
+++ src/fetch.js
@@ -1,3 +1,4 @@
+// example at https://gist.github.com/shapkarin/bc473e4d3f944a57ecb9b1ab2e3dc719
 import { put, call } from 'redux-saga/effects';
 
 export default function* fetch ({ action, method, start, succes, error }){
