d0830977dceae0a7cee78b52f8e8a30bc9b2f1d6
Yury Shapkarin <yury@shapkarin.me>
2019-03-19 00:07:25 +0300

    pre

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Router, Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import createHistory from 'history/createBrowserHistory';
 import { hot } from 'react-hot-loader';
 
+import store from './store';
 import Menu from 'Components/Menu';
 import Github from 'Pages/Github';
 import Projects from 'Pages/Projects'
 
-export default hot(module)(function ({ some, prop }) {
+const App = function () {
     return (
-        <>
+        <Provider store={ store }>
+          <Router history={ createHistory() }>
             <Menu />
-
             <Switch>
                 <Route
                 path="/github"
@@ -22,6 +26,9 @@ export default hot(module)(function ({ some, prop }) {
                 component={ Projects }
                 />
             </Switch>
-        </>
+          </Router>
+        </Provider>
     );
-})
+};
+
+export default App;
