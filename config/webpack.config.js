647a83716413703f927c2157bb3e50510ff9310a
Yury Shapkarin <yury@shapkarin.me>
2019-03-20 09:28:13 +0300

    webpack aliases

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -264,7 +264,10 @@ module.exports = function(webpackEnv) {
       alias: {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-        'react-native': 'react-native-web'
+        'react-native': 'react-native-web',
+        Pages: path.resolve(__dirname, './src/Pages'),
+        Components: path.resolve(__dirname, './src/Components'),
+        Utils: path.resolve(__dirname, './src/Utils'),
       },
       plugins: [
         // Adds support for installing with Plug'n'Play, leading to faster installs and adding
