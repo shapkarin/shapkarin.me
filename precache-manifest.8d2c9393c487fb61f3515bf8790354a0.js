4fd29a3a294823a42dd3a6c573d2c5191a6c1695
Yury Shapkarin <yury@shapkarin.me>
2019-05-01 01:15:11 +0300

    update

--- precache-manifest.8d2c9393c487fb61f3515bf8790354a0.js
+++ precache-manifest.8d2c9393c487fb61f3515bf8790354a0.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.fdfcfda2.js"
   },
   {
-    "revision": "75252712ebc005859a20",
-    "url": "/static/js/main.75252712.chunk.js"
+    "revision": "6cc63a123643dfa11e25",
+    "url": "/static/js/main.6cc63a12.chunk.js"
   },
   {
     "revision": "e8f8def800b27346121c",
     "url": "/static/js/2.e8f8def8.chunk.js"
   },
   {
-    "revision": "a412b4259dee1e3a555d393e5cae34e8",
+    "revision": "184983c5124ac8fc98d69963ad9eab2e",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6552eb6eee607752186db760e14695cb.js"
+  "/precache-manifest.8d2c9393c487fb61f3515bf8790354a0.js"
 );
 
 workbox.clientsClaim();
