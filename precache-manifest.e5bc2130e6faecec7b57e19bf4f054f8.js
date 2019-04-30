958cdcac838138816adce075058472ac460bbf30
Yury Shapkarin <yury@shapkarin.me>
2019-05-01 02:01:27 +0300

    update

---
+++ precache-manifest.e5bc2130e6faecec7b57e19bf4f054f8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "195b5d559c5734f42474",
+    "url": "/static/js/main.195b5d55.chunk.js"
+  },
+  {
+    "revision": "64e14f04870ab02e9fb5",
+    "url": "/static/js/2.64e14f04.chunk.js"
+  },
+  {
+    "revision": "6f0910699fefd97bbc4f74644fc4fba9",
+    "url": "/index.html"
+  }
+];
