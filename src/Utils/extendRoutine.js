6bd254a1ad1d265d58cf707eb3a9a80777e8d58f
Yury Shapkarin <yury@shapkarin.me>
2019-06-09 22:22:57 +0300

    remove useless by now

--- src/Utils/extendRoutine.js
+++ src/Utils/extendRoutine.js
@@ -1,31 +1,14 @@
 import { createAction } from 'redux-actions';
 
-const isFunction = value => typeof value === 'function';
 
-const getCreatorForType = (type, creator) => {
-  if (!creator) {
-    return creator;
-  }
-  if (isFunction(creator[type])) {
-    return creator[type];
-  }
-  if (isFunction(creator[type.toLowerCase()])) {
-    return creator[type.toLowerCase()];
-  }
-  if (isFunction(creator)) {
-    return creator;
-  }
-  return undefined;
-};
+export const createActionCreator = ({ type, typePrefix }) => createAction(`${typePrefix}/${type}`);
 
-export const createActionCreator = ({ type, typePrefix, payloadCreator, metaCreator }) => createAction(`${typePrefix}/${type}`, getCreatorForType(type, payloadCreator), getCreatorForType(type, metaCreator));
-
-export default function extendRoutine(routine, types = [], payloadCreator, metaCreator){
+export default function extendRoutine(routine, types = []){
   const triggerType = routine.toString();
   const typePrefix = triggerType.substring(0, triggerType.lastIndexOf('/'));
   return types.reduce(
     (result, stage) => {
-      const actionCreator = createActionCreator({ type: stage, typePrefix, payloadCreator, metaCreator });
+      const actionCreator = createActionCreator({ type: stage, typePrefix });
       return Object.assign(result, {
         [stage.toLowerCase()]: actionCreator,
         [stage.toUpperCase()]: actionCreator.toString(),
