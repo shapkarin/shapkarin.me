07460f3330e3cc3155e3870ab9a1f84a419002bb
Yury Shapkarin <yury@shapkarin.me>
2019-06-11 12:14:49 +0300

    rename

--- src/Pages/Projects/routines.js
+++ src/Pages/Projects/routines.js
@@ -2,4 +2,4 @@ import { createRoutine } from 'redux-saga-routines';
 import extendRoutine from 'extend-saga-routines';
 
 export const projects = extendRoutine(createRoutine('projects'), 'TOGGLE_PROJECT_INFO');
-export const info = createRoutine('info');
+export const info = createRoutine('projects/info');
