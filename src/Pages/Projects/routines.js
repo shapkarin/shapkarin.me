1485a547ccd4a84eaf04e16cf9ff77a1e722478c
Yury Shapkarin <yury@shapkarin.me>
2019-07-13 15:35:40 +0300

    add createExtendedRoutine

--- src/Pages/Projects/routines.js
+++ src/Pages/Projects/routines.js
@@ -1,5 +1,5 @@
 import { createRoutine } from 'redux-saga-routines';
-import extendRoutine from 'extend-saga-routines';
+import { createExtendedRoutine } from 'extend-saga-routines';
 
 export const projects = createRoutine('projects');
-export const info = extendRoutine(createRoutine('projects/info'), 'TOGGLE');
+export const info = createExtendedRoutine('projects/info', 'TOGGLE');
