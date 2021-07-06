4ad51821f7e8e406f993c3aebf81025b2b5f251a
Yury Shapkarin <yury@shapkarin.me>
2021-07-06 23:45:37 +0300

    revert to SPA routing support

---
+++ build/precache-manifest.c9f50d3030a03df1e6e17cf133177927.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9832720561962c803099",
+    "url": "/static/js/main.98327205.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a9d9801aaf69f3317eea",
+    "url": "/static/js/2.a9d9801a.chunk.js"
+  },
+  {
+    "revision": "c2052feb36a2d1e7c7b24ee66b448dd6",
+    "url": "/index.html"
+  }
+];
