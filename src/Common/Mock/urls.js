899ba53f6f5a3159319e79cddbb08faa4e84813a
Yury Shapkarin <yury@shapkarin.me>
2021-09-24 02:37:48 +0300

    try to use gh-pages as json api

--- src/Common/Mock/urls.js
+++ src/Common/Mock/urls.js
@@ -3,17 +3,16 @@ import { pathToRegexp } from 'path-to-regexp';
 
 // const REACT_APP_BACKEND_URL = 'https://shapkarin.me';
 // const BACKEND_URL = 'https://shapkarin.me';
-const BACKEND_URL = '';
 
 const backend = {
-  about: `${BACKEND_URL}/about`,
+  about: '/api/about.json',
   packages: {
-    _root: `${BACKEND_URL}/packages`,
+    _root: '/packages',
     package() {
       return pathToRegexp(`${this._root}/:id`);
     },
   },
-  sketches: `${BACKEND_URL}/sketches`,
+  sketches: '/sketches',
 }
 
 export default {
