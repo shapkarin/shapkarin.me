1b83f6c4c59ffe5ea3163d8246c7b4d9a2b0ee52
Yury Shapkarin <yury@shapkarin.me>
2022-12-21 10:53:10 +0300

    upd

--- src/Components/About/Website.js
+++ src/Components/About/Website.js
@@ -7,11 +7,12 @@ export default function AboutWebsite() {
 
     return  (
       <Fragment>
-        {!isMoreInfo && <div className="Link" onClick={() => setIsMoreInfo(true)}>[About website]</div>}
+        {!isMoreInfo && <Fragment><br/><div className="Link" onClick={() => setIsMoreInfo(true)}>{'---> About website <---'}</div></Fragment>}
         <Collapse open={isMoreInfo}>
           <div style={{ marginTop: 10 }}>
             <hr />
             <div className="p">
+              UPD: Will be rewritten to next.js <br/>
               Website dependecies React, React Hooks, React Query, Github API <a href="https://github.com/shapkarin/shapkarin.me/blob/master/package.json" target="_blank" rel="noreferrer">and other</a><br />
               Persional JSON API is <a href="https://github.com/shapkarin/shapkarin.me/tree/master/src/Generate-Backend" target="_blank" rel="noreferrer">generating</a>{' '}
               from <a href="https://github.com/shapkarin/shapkarin.me/blob/master/src/Generate-Backend/database.js" target="_blank" rel="noreferrer">JS structures</a> and serve as a static JSON files(<a href="https://github.com/shapkarin/shapkarin.me/tree/gh-pages/api/packages/info" target="_blank" rel="noreferrer">example</a>).<br />
