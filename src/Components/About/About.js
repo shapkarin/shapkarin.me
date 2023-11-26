8dfedb52e09bc43fe0c33fc8856db98d0343f49e
Yury Shapkarin <yury@shapkarin.me>
2023-11-26 06:16:53 +0300

    remove

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -27,9 +27,6 @@ function About() {
         </Formatted>
         <ul>
           <li>
-            <a href="https://drive.google.com/file/d/155imY9utcyQEj68yxZL3V0ygnyjlwpMw/view" target="_blank" rel="noreferrer">My CV in PDF at Google Drive <FiExternalLink /></a>
-          </li>
-          <li>
             <a href="https://drive.google.com/file/d/1by9rcRYvHE_gmLLM5BH71XhC8L_W4KLd/view" target="_blank" rel="noreferrer">Developer Story <FiExternalLink /></a>
             {!isCopied ? ' ' : '. '}
             {
