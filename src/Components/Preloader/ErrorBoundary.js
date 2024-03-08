fe87dedd6b802b45dc87208e04c26542480face9
Yury Shapkarin <yury@shapkarin.me>
2024-03-08 20:25:13 +0300

    feat: better globabl error message and loging #85

--- src/Components/Preloader/ErrorBoundary.js
+++ src/Components/Preloader/ErrorBoundary.js
@@ -11,13 +11,11 @@ class ErrorBoundary extends Component {
   }
   render() {
     if (this.state.hasError) {
-      return <div style={{ color: 'red', fontSize: 18, padding: 10 }}>
+      return <div style={{ color: 'red', fontSize: 18, padding: 10, width: 742 }}>
         Could not fetch :-(<br />
-        { this.state.error?.message && <div>Error message: { this.state.error.message }</div>}
-        Please, let me know <a href="mailto:yury@shapkarin.me?subject=Home Page Error">yury@shapkarin.me</a>
-        <br />
       </div>;
     }
+    console.log(JSON.stringify(this.state.error, null, 4))
     return this.props.children;
   }
 }
