f45d021afec0e5f7bbfb68a94d1cd164693bac09
Yury Shapkarin <yu.shapkarin@gmail.com>
2025-05-18 19:20:40 +0300

    fix: update GitHub Actions Link

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -18,7 +18,7 @@ function About() {
           <li><a  href={github.link} target="_blank" rel="noreferrer">{github.text}<FiExternalLink /></a></li>
         </ul>
         {/* yup `<br/><br/> is not good markup` https://github.com/shapkarin/shapkarin.me/tree/main?tab=readme-ov-file#NOTE-2 */}
-        The API for this site <a href="https://github.com/shapkarin/shapkarin.me/tree/master/src/Generate-Backend" target="_blank" rel="noreferrer">is generated</a><FiExternalLink /> and stored as <a href="https://github.com/shapkarin/shapkarin.me/tree/gh-pages/api" target="_blank" rel="noreferrer">JSON and Markdown</a><FiExternalLink /> on <a href="https://github.com/shapkarin/shapkarin.me/tree/main/.github/workflows/deploy.yml">GitHub Pages</a><br/>
+        The API for this site <a href="https://github.com/shapkarin/shapkarin.me/tree/master/src/Generate-Backend" target="_blank" rel="noreferrer">is generated</a><FiExternalLink /> and stored as <a href="https://github.com/shapkarin/shapkarin.me/tree/gh-pages/api" target="_blank" rel="noreferrer">JSON and Markdown</a><FiExternalLink /> on <a href="https://github.com/shapkarin/shapkarin.me/blob/main/.github/workflows/build-deploy.yml">GitHub Pages</a><br/>
         It also uses Github Actions to help with CI, and generates a static version,
         {' '}<a href={website.link} target='_blank' rel='noreferrer'>{website.text}<FiExternalLink /></a><br/>
         Website since 2013 {'=> '}<a href="https://web.archive.org/web/20250000000000*/shapkarin.me" target="_blank" rel="noreferrer">Proof<FiExternalLink /></a><br/>
