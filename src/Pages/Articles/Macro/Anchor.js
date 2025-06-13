897adf2672309f610e7ee7a55ab9f334c5b5e7f3
Yury Shapkarin <yu.shapkarin@gmail.com>
2025-06-13 03:25:22 +0300

    refactor: anchor md macro

--- src/Pages/Articles/Macro/Anchor.js
+++ src/Pages/Articles/Macro/Anchor.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import { Children, createElement } from 'react';
 
 const flatten = (text, child) => {
   return typeof child === 'string'
     ? text + child
-    : React.Children.toArray(child.props.children).reduce(flatten, text);
+    : Children.toArray(child.props.children).reduce(flatten, text);
 };
 
 const HeadingRenderer = props => {
-  const children = React.Children.toArray(props.children);
+  const children = Children.toArray(props.children);
   const text = children.reduce(flatten, '');
   const slug = text.toLowerCase().replace(/\W+/g, (match, index, str) => 
     index + match.length === str.length ? '' : '-'
   );
-  return React.createElement('h' + props.level, { id: slug }, props.children);
+  return createElement('h' + props.level, { id: slug }, props.children);
 };
 
 export default HeadingRenderer;
