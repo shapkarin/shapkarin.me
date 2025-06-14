c2af96e4c3f104bc9b51ec041bfb25e9ee229772
Yury Shapkarin <yu.shapkarin@gmail.com>
2025-06-14 04:25:36 +0300

    style: packages page simplification

--- src/Pages/Packages/Additional.js
+++ src/Pages/Packages/Additional.js
@@ -1,16 +1,13 @@
 import { FiExternalLink } from 'react-icons/fi';
 
 export default function Additional() {
-  return <div>
-    <a
+  return <a
       className="PagePackages__Item_more"
-      style={{ width: '185px' }}
       href="https://www.npmjs.com/~shapkarin"
       target="_blank" rel="noreferrer"
     >
-      Npm packages
+      https://www.npmjs.com/~shapkarin
       {' '}
       <FiExternalLink />
     </a>
-  </div>
 }
