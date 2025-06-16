327bf4c869d6a568a4b4d0eaba7c4bb4d44b7d45
Yury Shapkarin <yu.shapkarin@gmail.com>
2025-06-16 21:44:01 +0300

    fix: barberpole-animation, animation for npm packages external link, headings and other patch updates

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -22,7 +22,9 @@ function About() {
         <a href={linkedin.link} className="Social_Link" target="_blank" rel="noreferrer">{linkedin.text}<FiExternalLink /></a>
         <a href={github.link} className="Social_Link" target="_blank" rel="noreferrer">{github.text}<FiExternalLink /></a>
         <br />
-        <em>Disclaimer: This project is only a homepage, not an example of application architecture. <a href="https://github.com/shapkarin/shapkarin.me" target="_blank" rel="noreferrer">(git<FiExternalLink />)</a></em>
+        <em className="Disclaimer">
+          Disclaimer: gradually developed <a href="https://web.archive.org/web/20130801000000*/shapkarin.me" target="_blank" rel="noreferrer">since 2013<FiExternalLink /></a> with limited time, is not an example of application architecture {' | '} <a href="https://github.com/shapkarin/shapkarin.me" target="_blank" rel="noreferrer">cource code<FiExternalLink /></a>
+        </em>
         <Menu />
       </div>
     </>
