e514a56e04b53be2834a3f8fb94d944baa07bad4
Yury Shapkarin <yu.shapkarin@gmail.com>
2025-06-19 22:54:43 +0300

    style: improve design rhythm

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,12 +1,14 @@
 import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
 import clsx from 'clsx';
+import isMobile from 'is-mobile';
 
 import { FiExternalLink } from 'react-icons/fi';
 import { fetchAbout } from 'Common/API';
 import Preloader from 'Components/Preloader';
 
 import './style.less';
+
 
 function About() {
   const { data: { data: { title, intro, links: { linkedin, github } } } } = useQuery('About', () => fetchAbout());
@@ -18,9 +20,11 @@ function About() {
         })}>
         <Link to="/" className='About_greeting'>{title}</Link>
         {intro}
-        <a href={linkedin.link} className="Social_Link" target="_blank" rel="noreferrer">{linkedin.text}<FiExternalLink /></a>
+        {isMobile() && <br />}
+        <a href={linkedin.link} className={clsx('Social_Link', {
+          'Social_Link__mobile': isMobile()
+        })} target="_blank" rel="noreferrer">{linkedin.text}<FiExternalLink /></a>
         <a href={github.link} className="Social_Link" target="_blank" rel="noreferrer">{github.text}<FiExternalLink /></a>
-        <br />
         <em className="Disclaimer">
           Disclaimer: gradually developed <a href="https://web.archive.org/web/20130801000000*/shapkarin.me" target="_blank" rel="noreferrer">since 2013<FiExternalLink /></a> with limited time, is not an example of application architecture {' | '} <a href="https://github.com/shapkarin/shapkarin.me" target="_blank" rel="noreferrer">cource code<FiExternalLink /></a>
         </em>
