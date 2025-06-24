5aaba3fa0a849ba21cdea4badf6d35ea37167a99
Yury Shapkarin <yu.shapkarin@gmail.com>
2025-06-25 00:49:45 +0300

    docs: better link to GitHub Actions

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,8 +4,8 @@ import './style.less';
 
 const Footer = () =>
     <footer className="Footer">
-        The API for this site <a href="https://github.com/shapkarin/shapkarin.me/tree/master/src/Generate-Backend" target="_blank" rel="noreferrer">is generated</a><FiExternalLink /> and stored as <a href="https://github.com/shapkarin/shapkarin.me/tree/gh-pages/api" target="_blank" rel="noreferrer">JSON and Markdown</a><FiExternalLink /> on <a href="https://github.com/shapkarin/shapkarin.me/blob/main/.github/workflows/build-deploy.yml" target="_blank" rel="noreferrer">GitHub Pages<FiExternalLink /></a><br />
-        It uses Github Actions to help with CI, and generates a static version.
+        The API for this site <a href="https://github.com/shapkarin/shapkarin.me/tree/master/src/Generate-Backend" target="_blank" rel="noreferrer">is generated</a><FiExternalLink /> and stored as <a href="https://github.com/shapkarin/shapkarin.me/tree/gh-pages/api" target="_blank" rel="noreferrer">JSON and Markdown</a><FiExternalLink /> on GitHub Pages.<br />
+        It <a href="https://github.com/shapkarin/shapkarin.me/blob/main/.github/workflows/build-deploy.yml" target="_blank" rel="noreferrer">uses Github Actions<FiExternalLink /></a> to help with CI and generates a static version.
     </footer>
 
 export default Footer;
