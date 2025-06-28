273d23f838b80fbf1411764b5ca6f3adefdc0302
Yury Shapkarin <yu.shapkarin@gmail.com>
2025-06-28 07:39:15 +0300

    fix(mobile): don't init background genart on mobile

--- src/Components/RandomButton/RandomButton.js
+++ src/Components/RandomButton/RandomButton.js
@@ -2,6 +2,7 @@ import scene from '../Background';
 
 const RandomButton = ({ children, ...rest }) => {
   const randomize = function(){
+    if(!scene) return;
     scene.randomizeAll();
     scene.draw();
   };
