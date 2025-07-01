86bd414e5a0a45295276ab3690d01101fd0789e7
Yury Shapkarin <yu.shapkarin@gmail.com>
2025-07-01 20:13:15 +0300

    refactor: use getters for functions without args
    TODO: branch refactor/fn

--- src/API/urls.js
+++ src/API/urls.js
@@ -1,19 +1,20 @@
 const GITHUB_API_URL = 'https://api.github.com';
 
 const github = {
-  user() {
+  get user() {
     return `${GITHUB_API_URL}/users/shapkarin`;
   },
-  activity() {
-    return `${this.user()}/events/public?per_page=100`;
+  get activity() {
+    return `${this.user}/events/public?per_page=100`;
   },
-  repositories(n = 1) {
+  get repositories() {
     // TODO: pagination
-    const PER_PAGE = 64;
-    return `${this.user()}/repos?sort=updated&per_page=${PER_PAGE}&page=${n}`;
+    const perPage = 64;
+    const page = 1;
+    return `${this.user}/repos?sort=updated&per_page=${perPage}&page=${page}`;
   },
-  likes() {
-    return `${this.user()}/starred`;
+  get likes() {
+    return `${this.user}/starred`;
   },
 }
 
