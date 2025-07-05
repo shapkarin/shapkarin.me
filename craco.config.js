23f253f826cd3b217b026feace914fe83b20dab2
Yury Shapkarin <yu.shapkarin@gmail.com>
2025-07-06 02:00:07 +0300

    feat(articles): add markdown tables support

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,22 @@
 const path = require("path");
 const CracoLessPlugin = require("craco-less");
+const webpack = require("webpack");
 
 module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+    resolve: {
+      fallback: {
+        "buffer": require.resolve("buffer/")
+      }
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+      }),
+    ],
   },
   plugins: [{ plugin: CracoLessPlugin }],
 }
