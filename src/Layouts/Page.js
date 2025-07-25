66b56e57fd501be7f8db04411ac2eb6b56000487
Yury Shapkarin <yu.shapkarin@gmail.com>
2025-07-25 14:52:09 +0300

    feat: hide close button on main page

--- src/Layouts/Page.js
+++ src/Layouts/Page.js
@@ -1,14 +1,19 @@
+import { useLocation } from 'react-router-dom';
+
 import Close from "@/Components/Close";
 import Preloader from "@/Components/Preloader";
 import ScrollToTop from '@/Components/ScrollToTop';
 
 // TODO: refact
-const PageLayout = ({ children }) => (
-  <Preloader>
-    <Close />
+const PageLayout = ({ children }) => {
+  const location = useLocation();
+  return (
+    <Preloader>
+      {location.pathname !== '/' && <Close />}
     {children}
     <ScrollToTop />
   </Preloader>
-);
+  );
+};
 
 export default PageLayout;
