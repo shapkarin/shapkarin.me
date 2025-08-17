d071a19725594cb3c89d2239b5ffb2b8de85cc73
Uri Kiro <yu.shapkarin@gmail.com>
2025-08-18 00:43:38 +0300

    fix: use react hydrate due to static

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { hydrate } from 'react-dom';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const rootElement = document.getElementById('root');
+hydrate(<App />, rootElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
