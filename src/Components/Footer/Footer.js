02db90589896576d7dfcbc8a3c451490b4ae5cfb
Uri Kiro <yu.shapkarin@gmail.com>
2025-10-12 21:07:44 +0300

    docs: fix typo in link to generated API docs

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './style.less';
 
 const Footer = () =>
     <footer className="Footer">
-        The API for this site <a href="https://github.com/shapkarin/shapkarin.me/tree/master/src/Generate-Backend" target="_blank" rel="noreferrer">is generated</a><FiExternalLink /> and stored as <a href="https://github.com/shapkarin/shapkarin.me/tree/gh-@/Pages/api" target="_blank" rel="noreferrer">JSON and Markdown</a><FiExternalLink /> on GitHub Pages.<br />
+        The API for this site <a href="https://github.com/shapkarin/shapkarin.me/tree/master/src/Generate-Backend" target="_blank" rel="noreferrer">is generated</a><FiExternalLink /> and stored as <a href="https://github.com/shapkarin/shapkarin.me/tree/gh-pages/api" target="_blank" rel="noreferrer">JSON and Markdown</a><FiExternalLink /> on GitHub Pages.<br />
         It <a href="https://github.com/shapkarin/shapkarin.me/blob/main/.github/workflows/build-deploy.yml" target="_blank" rel="noreferrer">uses Github Actions<FiExternalLink /></a> to help with CI and generates a static version.
     </footer>
 
