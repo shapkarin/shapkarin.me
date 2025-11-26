4433c04ccc828c174a818eb198640cfe451db994
Uri Kiro <yu.shapkarin@gmail.com>
2025-11-26 23:24:04 +0300

    docs: Github Actions link to the folder instead of file

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ import './style.less';
 const Footer = () =>
     <footer className="Footer">
         The API for this site <Link href="https://github.com/shapkarin/shapkarin.me/tree/master/src/Generate-Backend">is generated</Link> and stored as <Link href="https://github.com/shapkarin/shapkarin.me/tree/gh-pages/api">JSON and Markdown</Link> on GitHub Pages.<br />
-        It <Link href="https://github.com/shapkarin/shapkarin.me/blob/main/.github/workflows/build-deploy.yml">uses Github Actions</Link> to help with CI and generates a static version.
+        It <Link href="https://github.com/shapkarin/shapkarin.me/blob/main/.github/workflows">uses Github Actions</Link> to help with CI and generates a static version.
     </footer>
 
 export default Footer;
