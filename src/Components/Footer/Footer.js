16149147eea747410f12a2c20b47de8a87d5fef6
Uri Kiro <yu.shapkarin@gmail.com>
2026-03-15 15:53:20 +0300

    feat: render latest commit hash instead of semVer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ import './style.less';
 const Footer = () =>
     <footer className="Footer">
         The API for this site <Link href="https://github.com/shapkarin/shapkarin.me/tree/master/src/Generate-Backend">is generated</Link> and stored as <Link href="https://github.com/shapkarin/shapkarin.me/tree/gh-pages/api">JSON and Markdown</Link> on GitHub Pages.<br />
-        It <Link href="https://github.com/shapkarin/shapkarin.me/blob/main/.github/workflows">uses Github Actions</Link> to help with CI and generates a static version.
+        It <Link href="https://github.com/shapkarin/shapkarin.me/blob/main/.github/workflows">uses Github Actions</Link> to help with CI and generates a static version. Latest commit: <Link href={`https://github.com/shapkarin/shapkarin.me/commit/${process.env.REACT_APP_COMMIT_HASH}`}>{process.env.REACT_APP_COMMIT_HASH && process.env.REACT_APP_COMMIT_HASH.slice(0, 7)}</Link>
     </footer>
 
 export default Footer;
