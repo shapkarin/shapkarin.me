d04f52b0b964fe6e3d76f848d897c96c0871a286
Uri Kiro <yu.shapkarin@gmail.com>
2026-04-20 23:17:22 +0300

    feat!: remove commit hash form scripts and footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,11 @@
 import Link from '@/Components/Link';
-import { GITHUB_REPO_URL, COMMIT_HASH } from '@/constants';
+import { GITHUB_REPO_URL } from '@/constants';
 
 import './style.less';
 
 const Footer = () =>
     <footer className="Footer">
-        <Link href={`${GITHUB_REPO_URL}/blob/main/.github/workflows`}>Github Actions</Link> to help with CI, generate API as flat files and static version. Latest commit: <Link href={`${GITHUB_REPO_URL}/commit/${COMMIT_HASH}`}>{COMMIT_HASH && COMMIT_HASH.slice(0, 7)}</Link>
+        <Link href={`${GITHUB_REPO_URL}/blob/main/.github/workflows`}>Github Actions</Link> to help with CI, generate API as flat files and static version.
     </footer>
 
 export default Footer;
--- src/constants.js
+++ src/constants.js
@@ -5,4 +5,3 @@ export const isMobile = isMobileLib();
 export const GITHUB_REPO_URL = 'https://github.com/shapkarin/shapkarin.me';
 
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
-export const COMMIT_HASH = process.env.REACT_APP_COMMIT_HASH;
