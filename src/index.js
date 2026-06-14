9b681229757c9b529373d0d2558cf43e4690a14b
Uri Kiro <yu.shapkarin@gmail.com>
2026-06-14 09:44:05 +0200

    chore: add local only vitals

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { createRoot, hydrateRoot } from 'react-dom/client';
 import App from './App';
-import { IS_PRODUCTION } from '@/constants';
-// import reportWebVitals from './reportWebVitals';
+import { IS_PRODUCTION, IS_DEVELOPMENT } from '@/constants';
 
 const rootElement = document.getElementById('root');
 const GH_PAGES_CANT_HYDRATE = true; // turn on `hydrateRoot` to test
@@ -11,7 +10,8 @@ IS_PRODUCTION && GH_PAGES_CANT_HYDRATE
   ? hydrateRoot(rootElement, <App />)
   : createRoot(rootElement).render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+if (IS_DEVELOPMENT) {
+  import('./reportWebVitals').then(({ reportWebVitals }) => {
+    reportWebVitals(metric => console.info('[web-vitals]', metric));
+  });
+}
--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,24 @@
-// const reportWebVitals = onPerfEntry => {
-//   if (onPerfEntry && onPerfEntry instanceof Function) {
-//     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-//       getCLS(onPerfEntry);
-//       getFID(onPerfEntry);
-//       getFCP(onPerfEntry);
-//       getLCP(onPerfEntry);
-//       getTTFB(onPerfEntry);
-//     });
-//   }
-// };
+/*
+  I DON'T use any analytics on my homepage, and I DON'T track your data.
+  I do use ONLY Google Search Console. It's basically Google Webmaster Tools,
+  not webpage analytics that send your data somewhere else.
+  
+  Search Console has its own Core Web Vitals report for indexed pages. 
+  
+  => The code below is local only! <=
 
-// export default reportWebVitals;
+  Note: it can take a bit of time to generate the local report, and ofc the `Preserve log` option is useful.
+*/
+export const reportWebVitals = onPerfEntry => {
+  if (typeof onPerfEntry !== 'function') {
+    return;
+  }
+
+  import('web-vitals')
+    .then(({ onCLS, onINP, onLCP }) => {
+      onCLS(onPerfEntry);
+      onINP(onPerfEntry);
+      onLCP(onPerfEntry);
+    })
+    .catch(() => {});
+};
