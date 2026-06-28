d7262bdb7e06c763121d2ecc0811ae83dea8cb04
Uri Kiro <yu.shapkarin@gmail.com>
2026-06-28 02:12:18 +0200

    docs(packages): add legacy CLI tool link

--- src/Pages/Packages/Packages.js
+++ src/Pages/Packages/Packages.js
@@ -1,10 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { FiExternalLink } from 'react-icons/fi';
+import ReactTooltip from 'react-tooltip';
+
 import SEO from '@/Components/SEO';
+import Link from '@/Components/Link';
 
 import { fetchPackages } from '@/DAL';
 import PackageInfo from './PackageInfo';
 import Additional from './Additional';
+
 
 import './style.less';
 
@@ -19,6 +23,9 @@ export default function Packages() {
       <SEO 
         title="JavaScript & TypeScript NPM Packages | Yuri Shapkarin"
         description="Explore a collection of JavaScript, TypeScript, and Node.js NPM packages for modern web development. Open-source modules for Redux, React, and more."
+      />
+      <ReactTooltip
+        place="bottom"
       />
       <div className="PagePackages Page__Inner">
         {packages.map(({
@@ -37,6 +44,9 @@ export default function Packages() {
         ))}
         <Additional />
       </div>
+      <Link to="https://github.com/shapkarin/redux-scaffolder" data-tip="I unpublished it later but still visible via npm API">
+        Created the CLI tool in 2019
+      </Link>
     </>
   );
 };
